e63e652e63452646a883ea29cb2b6b85fa94f31b
MarKow98 <marcin.kowalski0@onet.pl>
2025-02-23 20:28:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ df = load_data()
 st.title("Sprawdź swój urlop")
 
 image_url = "https://d3npyywa6qnolf.cloudfront.net/prod/user/823265/eyJ1cmwiOiJodHRwczpcL1wvcGF0cm9uaXRlLnBsXC91cGxvYWRcL3VzZXJcLzgyMzI2NVwvYXZhdGFyX29yaWcuanBnPzE2OTIyNzU2NDciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjI5MCwib3B0aW9ucyI6eyJxdWFsaXR5Ijo5NX19LCJ0b0Zvcm1hdCI6ImpwZWcifX0%3D/Eu%2Fq%2FhM1o8HfA32iLnH6uBNYVnn1hI1idKxeyZDdcRY%3D"  # Podmień na rzeczywisty URL obrazka
-st.image(image_url, use_container_width=True)
+st.markdown(
+    f'<img src="{image_url}" width="300">',
+    unsafe_allow_html=True
+)
 
 # Łączenie kolumn Imię i Nazwisko
 df['Nazwisko i Imię'] = df['Nazwisko'] + " " + df['Imię']
